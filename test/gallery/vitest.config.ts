a3dc85c804ae8b7fc10df2c2ee38f1f02b06ea83
Andrew Fischer <andy.fischer@gmail.com>
2025-09-26 22:04:35 -0600

    update CI

--- test/gallery/vitest.config.ts
+++ test/gallery/vitest.config.ts
@@ -1,12 +1,9 @@
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
-const srcDir = fileURLToPath(new URL('./src', import.meta.url));
+const srcDir = fileURLToPath(new URL('../../src', import.meta.url));
 
 export default defineConfig({
-  test: {
-    exclude: ['**/node_modules/**', '**/test/gallery/**'],
-  },
   resolve: {
     alias: [
       {
